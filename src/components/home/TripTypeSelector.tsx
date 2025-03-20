10b307a83fc9b1f4d5b21828ded65f0012b2cbcf
Julianhima91 <hima.julian91@gmail.com>
2025-03-20 20:35:15 +0100

    Update TripTypeSelector.tsx

--- src/components/home/TripTypeSelector.tsx
+++ src/components/home/TripTypeSelector.tsx
@@ -16,7 +16,7 @@ export function TripTypeSelector({ tripType, onTripTypeChange }: TripTypeSelecto
         }`}
         onClick={() => onTripTypeChange('roundTrip')}
       >
-        Round Trip
+        Vajtje Ardhje
       </button>
       <button
         className={`px-3 py-2 text-sm font-medium rounded-r-lg ${
@@ -26,7 +26,7 @@ export function TripTypeSelector({ tripType, onTripTypeChange }: TripTypeSelecto
         }`}
         onClick={() => onTripTypeChange('oneWay')}
       >
-        One Way
+        Vajtje
       </button>
     </div>
   );
