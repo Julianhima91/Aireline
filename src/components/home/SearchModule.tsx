543a566661983e560a843f10b0bbda2b3fc5d018
Julianhima91 <hima.julian91@gmail.com>
2025-03-20 20:38:53 +0100

    Update SearchModule.tsx

--- src/components/home/SearchModule.tsx
+++ src/components/home/SearchModule.tsx
@@ -111,7 +111,7 @@ export function SearchModule({
                   onChange={(e) => setDirectFlightsOnly(e.target.checked)}
                   className="w-4 h-4 text-blue-600 border-gray-300 rounded focus:ring-blue-500"
                 />
-                <span className="text-gray-600">Show only direct flights</span>
+                <span className="text-gray-600">Shfaq Vetem Fluturime Direkte</span>
               </label>
             </div>
           </div>
