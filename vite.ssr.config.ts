83adcde4b2845fdec8e2167e26e65c0b23e845ff
Julianhima91 <hima.julian91@gmail.com>
2025-03-22 20:26:25 +0100

    Update vite.ssr.config.ts

--- vite.ssr.config.ts
+++ vite.ssr.config.ts
@@ -1,15 +1,1 @@
 // vite.ssr.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  ssr: {
-    noExternal: ['react-router-dom', 'react-helmet-async']
-  },
-  build: {
-    ssr: 'ssr-entry.ts',
-    sourcemap: true
-    // ❌ No manualChunks here!
-  }
-});
