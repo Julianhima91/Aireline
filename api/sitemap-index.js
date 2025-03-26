3cd3758aa137e9cc085fa0449e0b207bbdabbb24
Julianhima91 <hima.julian91@gmail.com>
2025-03-26 14:00:22 +0100

    Create sitemap-index.js

---
+++ api/sitemap-index.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+  const totalPages = 7; // Approximate, you can increase if needed
+  const baseUrl = 'https://biletaavioni.himatravel.com';
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${Array.from({ length: totalPages }).map((_, i) => `
+      <sitemap>
+        <loc>${baseUrl}/sitemaps/page-${i + 1}.xml</loc>
+      </sitemap>
+    `).join('')}
+  </sitemapindex>`;
+
+  res.setHeader('Content-Type', 'application/xml');
+  res.status(200).send(xml);
+}
